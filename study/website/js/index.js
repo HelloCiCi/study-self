142ab88c14e9bf3850bb14c4490ea6a86651828f
HelloCiCi <gaoyuxin12131@163.com>
2017-10-19 10:44:18 +0800

    个人网站

--- study/website/js/index.js
+++ study/website/js/index.js
@@ -2,3 +2,18 @@
  * Created by GYX on 17/10/15.
  */
 
+    var oNav = document.getElementById('nav');
+    var aLi = document.getElementsByTagName('li');
+    var aA = document.getElementsByTagName('a');
+
+
+
+
+
+    window.onscroll = function(){
+        oNav.className = 'scroll-nav';
+        for(var i = 0; i < aA.length; i++){
+            aLi[i].className = 'scroll-li';
+            aA[i].className = 'scroll-a';
+        }
+    }
