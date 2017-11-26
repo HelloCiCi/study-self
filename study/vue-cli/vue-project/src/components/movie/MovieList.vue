2fbb82b1dca95592c196de730653107fd353ac5e
HelloCiCi <gaoyuxin12131@163.com>
2017-11-26 23:23:38 +0800

    vue-cli练习

---
+++ study/vue-cli/vue-project/src/components/movie/MovieList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="Top250">
+    <div class= "img">
+      <img src="../../assets/logo.png">
+    </div>
+   <div class = "intru">
+     <span>{{title}}</span>
+     <p>
+       <span>{{des}}</span>
+       ({{year}}) ({{sor}})
+     </p>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['title','des','year','sor'],
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .Top250{
+    height: 1rem;
+    padding: 10px 10px 0px 10px;
+  }
+  .img{
+      float: left;
+      width: 1rem;
+      vertical-align: bottom;
+      
+  }
+
+  .intru{
+    float: left;
+    font-size: 0.2rem;
+    width: 4rem;
+    margin-left: 0.6rem;
+    border-bottom: 1px solid #ccc;
+  }
+</style>
